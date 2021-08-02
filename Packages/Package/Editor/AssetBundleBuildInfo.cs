0b008e786ab9480969ad1d15d887f07957fbc060
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-02 19:04:33 +0300

    Add AssetBundleBuildUtility

---
+++ Packages/Package/Editor/AssetBundleBuildInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace UGF.AssetBundles.Editor
+{
+    public class AssetBundleBuildInfo
+    {
+        public string Name { get; }
+        public string VariantName { get; }
+        public IReadOnlyDictionary<string, string> Assets { get; }
+
+        private readonly Dictionary<string, string> m_assets = new Dictionary<string, string>();
+
+        public AssetBundleBuildInfo(string name) : this(name, string.Empty)
+        {
+        }
+
+        public AssetBundleBuildInfo(string name, string variantName)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+
+            Name = name;
+            VariantName = variantName ?? throw new ArgumentNullException(nameof(variantName));
+            Assets = new ReadOnlyDictionary<string, string>(m_assets);
+        }
+
+        public void AddAsset(string address, string assetPath)
+        {
+            if (string.IsNullOrEmpty(address)) throw new ArgumentException("Value cannot be null or empty.", nameof(address));
+            if (string.IsNullOrEmpty(assetPath)) throw new ArgumentException("Value cannot be null or empty.", nameof(assetPath));
+
+            m_assets.Add(address, assetPath);
+        }
+
+        public bool RemoveAsset(string address)
+        {
+            if (string.IsNullOrEmpty(address)) throw new ArgumentException("Value cannot be null or empty.", nameof(address));
+
+            return m_assets.Remove(address);
+        }
+
+        public void ClearAssets()
+        {
+            m_assets.Clear();
+        }
+
+        public Dictionary<string, string>.Enumerator GetEnumerator()
+        {
+            return m_assets.GetEnumerator();
+        }
+    }
+}
---
+++ Packages/Package/Editor/AssetBundleBuildUtility.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.AssetBundles.Editor
+{
+    public static class AssetBundleBuildUtility
+    {
+        public static AssetBundleManifest Build(IReadOnlyList<AssetBundleBuildInfo> assetBundles, string outputPath, BuildTarget target, BuildAssetBundleOptions options = BuildAssetBundleOptions.None)
+        {
+            if (assetBundles == null) throw new ArgumentNullException(nameof(assetBundles));
+
+            AssetBundleBuild[] builds = GetAssetBundleBuilds(assetBundles);
+            AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(outputPath, builds, options, target);
+
+            return manifest;
+        }
+
+        public static AssetBundleBuild[] GetAssetBundleBuilds(IReadOnlyList<AssetBundleBuildInfo> assetBundles)
+        {
+            if (assetBundles == null) throw new ArgumentNullException(nameof(assetBundles));
+
+            var builds = new AssetBundleBuild[assetBundles.Count];
+
+            for (int i = 0; i < assetBundles.Count; i++)
+            {
+                AssetBundleBuildInfo info = assetBundles[i];
+
+                var build = new AssetBundleBuild
+                {
+                    assetBundleName = info.Name,
+                    assetBundleVariant = info.VariantName,
+                    addressableNames = new string[info.Assets.Count],
+                    assetNames = new string[info.Assets.Count],
+                };
+
+                int index = 0;
+
+                foreach (KeyValuePair<string, string> pair in info)
+                {
+                    build.addressableNames[index] = pair.Key;
+                    build.assetNames[index] = pair.Value;
+
+                    index++;
+                }
+
+                builds[i] = build;
+            }
+
+            return builds;
+        }
+    }
+}
