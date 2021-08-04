6f35dd718a0ccfe8a9487a81a63b11239dcb0c5d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-04 18:24:49 +0300

    Add TestAssetBundleHandler

---
+++ Assets/UGF.AssetBundles.Runtime.Tests/TestAssetBundleHandler.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace UGF.AssetBundles.Runtime.Tests
+{
+    public class TestAssetBundleHandler
+    {
+        [Test]
+        public void LoadAndUnload()
+        {
+            var handler = new AssetBundleHandler("Assets/StreamingAssets/AssetBundles/AssetBundle_1");
+
+            handler.Load();
+
+            Assert.True(handler.HasAssetBundle);
+            Assert.NotNull(handler.AssetBundle);
+
+            handler.Unload();
+
+            Assert.False(handler.HasAssetBundle);
+        }
+    }
+}
