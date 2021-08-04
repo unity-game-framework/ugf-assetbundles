8dc296ecaa0d0781a05edfb97546125652ece53b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-04 18:33:32 +0300

    Add TestAssetBundleScope

---
+++ Assets/UGF.AssetBundles.Runtime.Tests/TestAssetBundleScope.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UGF.AssetBundles.Runtime.Tests
+{
+    public class TestAssetBundleScope
+    {
+        [Test]
+        public void Scope()
+        {
+            using (var scope = new AssetBundleScope("Assets/StreamingAssets/AssetBundles/AssetBundle_2"))
+            {
+                Assert.True(scope.Handler.HasAssetBundle);
+                Assert.NotNull(scope.Handler.AssetBundle);
+                Assert.True(AssetBundle.GetAllLoadedAssetBundles().Any(x => x.name.StartsWith("8b70", StringComparison.OrdinalIgnoreCase)));
+            }
+
+            Assert.False(AssetBundle.GetAllLoadedAssetBundles().Any(x => x.name.StartsWith("8b70", StringComparison.OrdinalIgnoreCase)));
+        }
+    }
+}
