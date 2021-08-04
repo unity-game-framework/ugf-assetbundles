c206fbc303d4660eae2e299662ab5d0f85e34abb
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-04 18:33:25 +0300

    Add AssetBundleScope

---
+++ Packages/UGF.AssetBundles/Runtime/AssetBundleScope.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UGF.AssetBundles.Runtime
+{
+    public readonly struct AssetBundleScope : IDisposable
+    {
+        public AssetBundleHandler Handler { get; }
+
+        public AssetBundleScope(string path, bool load = true)
+        {
+            Handler = new AssetBundleHandler(path);
+
+            if (load)
+            {
+                Handler.Load();
+            }
+        }
+
+        public AssetBundleScope(AssetBundleHandler handler)
+        {
+            Handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        public void Dispose()
+        {
+            Handler.Unload();
+        }
+    }
+}
